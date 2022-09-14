b797b21d40349c5c8c79ef747bd3a756fbef2659
Oyewo Abubakri <abubakrilaitan@gmail.com>
2022-09-14 04:36:26 -0500

    architect-2

--- js/closure/index.js
+++ js/closure/index.js
@@ -40,3 +40,24 @@ d.addTask({ type: "I need to go to school", time: new Date().now });
 d.addTask({ type: "I need to go to class", time: new Date().now });
 d.addTask({ type: "I need to go home", time: new Date().now });
 d.listTask();
+
+const u = [
+  { name: 11, age: 22 },
+  { name: 12, age: 93 },
+  { name: 21, age: 23 },
+  { name: 61, age: 53 },
+  { name: 31, age: 13 },
+  { name: 31, age: 23 },
+  { name: 31, age: 13 },
+];
+
+const reducer = (prev, newValue, currentIndex, arr) => {
+  let value = {};
+  console.log(prev);
+  return [prev.age + newValue.age, prev.name + newValue.name];
+};
+const average = (u) => {
+  return u.reduce(reducer, { name: 0, age: 0 });
+};
+
+console.log(average(u));
